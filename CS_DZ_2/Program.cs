b7a361f37f0f36b053c77261802f554eb3590c57
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-06 10:15:53 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myGold;
+            int cristalCount;
+            int priseOfCrislals = 10;
+
+            Console.WriteLine("Приветстыую вас в магазине кристаллов! Цена за один кристалл - " + priseOfCrislals + " золота");
+            Console.Write("Сколько у вас золота? ");
+            myGold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов вы хотите купить? ");
+            cristalCount = Convert.ToInt32(Console.ReadLine());
+
+            myGold -= cristalCount * priseOfCrislals;
+
+            Console.WriteLine("Вы приобрели " + cristalCount + " кристаллов. У вас осталось " + myGold + " золота.");
+        }
+    }
+}
